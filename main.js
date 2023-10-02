67d35a38c4dd64facaf80a509db78a3c005581b3
Abdul-Mujib <mujibajimoh@gmail.com>
2023-10-02 14:49:37 -0700

    html and css for age calculator

---
+++ main.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('.form');
+const inputControl = document.querySelector('.inputcontrol');
+const submitBtn = document.querySelector('#button');
+
+// input 
+const dayInput = document.querySelector('#day');
+const monthInput = document.querySelector('#month');
+const yearInpuy = document.querySelector('#year');
+
+// output
+const yearOutput = document.querySelector('#year__output');
+const monthOutput = document.querySelector('#month__output');
+const dayOutput = document.querySelector('#day__output');
+
